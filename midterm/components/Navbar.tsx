91751829ba2371628eabeea8b0c04ee154b4f3a4
Luis V <89930344+alejjuuu@users.noreply.github.com>
2023-10-26 13:07:37 -0400

    adding the navbar closes #6, closes #14

--- midterm/components/Navbar.tsx
+++ midterm/components/Navbar.tsx
@@ -1,10 +1,40 @@
-import React from 'react';
-
 export default function Home() {
-    return (
-        <div>
-            <div className="container">
-            </div>
+  return (
+    <div>
+      <header className="ds-header" id="site-header">
+        <div className="container">
+          <div className="ds-header-inner">
+            {/* logo */}
+            <a href="index" className="ds-logo">
+              <span>A</span>lejandro Velasquez
+            </a>
+            {/* social */}
+            <li>
+              <a href="#" target="_blank">
+                <i className="ri-stack-overflow-line"></i>
+              </a>
+            </li>
+
+            <ul className="ds-social">
+              <li>
+                <a href="https://github.com/alejjuuu" target="_blank">
+                  <i className="ri-github-fill"></i>
+                </a>
+              </li>
+              <li>
+                <a
+                  href="https://www.linkedin.com/in/alejandro-velasquez-b4a9971b4/"
+                  target="_blank"
+                >
+                  <i className="ri-linkedin-fill"></i>
+                </a>
+              </li>
+            </ul>
+          </div>
         </div>
-            );
+      </header>
+
+      {/* Your other components and content can go here */}
+    </div>
+  );
 }
