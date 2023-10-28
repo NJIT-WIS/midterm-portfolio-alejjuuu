24ccaa71ce62fc1c747b18cbf141c0343ed135ab
Luis V <89930344+alejjuuu@users.noreply.github.com>
2023-10-28 16:28:55 -0400

    new changes, added a new banner, added a new css for the banner closes #32, closes #37

---
+++ midterm/components/Welcome.tsx
@@ -0,0 +1,52 @@
+import React from "react";
+import Image from "next/image";
+
+
+const Welcome: React.FC = () => {
+  return (
+    <div className="ds-banner">
+      <div className="container">
+        <div className="row">
+          <div className="col-12 col-sm-12 col-md-5 col-lg-5 col-xl-5 col-xxl-5">
+            <figure>
+              <Image
+                src="/../pages/assets/images/profile-image.jpg"
+                className="circle-image"
+                alt={""}
+                width={500}
+                height={500}
+              ></Image>
+            </figure>
+
+          </div>
+
+          <div className="col-12 col-sm-12 col-md-7 col-lg-7 col-xl-7 col-xxl-7">
+            <section>
+              <h1>
+                <span>Hello world</span>
+                I am Alejandro <br /> the BEST software engineer and sales man.
+              </h1>
+              <ul className="ds-numbervalulist">
+                <li>
+                  <h3>New Jersey Institute of Technology</h3>
+                  <span>Bachelor in Information Technology</span>
+                </li>
+                <br />
+                <li>
+                  <strong>3+</strong>
+                  <span>Experience</span>
+                </li>
+                <li>
+                  <strong>15</strong>
+                  <span>Projects</span>
+                </li>
+              </ul>
+            </section>
+          </div>
+        </div>
+      </div>
+    </div>
+  );
+};
+
+export default Welcome;
