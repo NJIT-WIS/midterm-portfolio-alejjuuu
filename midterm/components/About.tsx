2d0a45d95f931375d7eae6447c5eef0df9d9f665
Luis V <89930344+alejjuuu@users.noreply.github.com>
2023-10-28 18:08:21 -0400

    created a new about page

---
+++ midterm/components/About.tsx
@@ -0,0 +1,30 @@
+import React from "react";
+import Link from "next/link";
+
+const About: React.FC = () => {
+  return (
+    <div className="ds-about-section">
+      <div className="container">
+        <section>
+          <h2 className="ds-heading">About Me</h2>
+          <p>
+            Hard worker, thirsty for learning, hustler, self-driven, looking for
+            technical sales experience.
+          </p>
+          <div className="ds-button-sec text-center">
+            <a
+              href="https://docs.google.com/document/d/1QPQSt-Kqm7OJtgIgJ-GQV-lzIVm0j6sVt2dPkNTtcvQ/edit?usp=sharing"
+              className="ds-button"
+              target="_blank"
+              rel="noopener noreferrer"
+            >
+              Download Resume
+            </a>
+          </div>
+        </section>
+      </div>
+    </div>
+  );
+};
+
+export default About;
--- midterm/pages/index.tsx
+++ midterm/pages/index.tsx
@@ -1,13 +1,16 @@
 import Navbar from "../components/Navbar";
 import Footer from "../components/Footer";
 import Welcome from "../components/Welcome";
+import About from "../components/About";
 
 export default function Home() {
   return (
     <div>
       <Navbar />
       <Welcome />
+      <About/>
       <Footer />
+
       {/* Your other components and content*/}
     </div>
   );
