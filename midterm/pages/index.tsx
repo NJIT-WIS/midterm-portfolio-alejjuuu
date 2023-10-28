ea42b85ece01e8d6203e3a5db063ed524f34e0ed
Luis V <89930344+alejjuuu@users.noreply.github.com>
2023-10-28 16:28:55 -0400

    new changes, added a new banner, added a new css for the banner closes #32, closes #37

--- midterm/pages/index.tsx
+++ midterm/pages/index.tsx
@@ -1,12 +1,13 @@
 import Navbar from "../components/Navbar";
 import Footer from "../components/Footer";
+import Welcome from "../components/Welcome";
 
 export default function Home() {
   return (
     <div>
       <Navbar />
+      <Welcome />
       <Footer />
-
       {/* Your other components and content*/}
     </div>
   );
