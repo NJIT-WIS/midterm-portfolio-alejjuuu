7eb4508f5cd1ff9f85caa02a2a9cac16ccc9726f
Luis V <89930344+alejjuuu@users.noreply.github.com>
2023-11-02 23:41:03 -0400

    fixed the issues and added tags on the <a> closes #62

--- midterm/pages/_app.tsx
+++ midterm/pages/_app.tsx
@@ -1,4 +1,5 @@
 import { AppProps } from "next/app";
+import '../styless/globals.css'; // Import the global CSS
 import Layout from "./layout"; // If you are using a layout component
 
 function MyApp({ Component, pageProps }: AppProps) {
