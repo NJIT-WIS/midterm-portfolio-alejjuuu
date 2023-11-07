08c03073bd17cd534826f710fbbd2ab7822f73c9
Luis V <89930344+alejjuuu@users.noreply.github.com>
2023-11-06 20:53:28 -0500

    new pages pushed

--- midterm/next.config.js
+++ midterm/next.config.js
@@ -1,6 +1,8 @@
 module.exports = {
   images: {
     domains: ['media.licdn.com'],
+    unoptimized: true, // Disable the image optimization
   },
+  assetPrefix: './', // This is necessary if your site is hosted on GitHub Pages
   // ... other Next.js config
 };
