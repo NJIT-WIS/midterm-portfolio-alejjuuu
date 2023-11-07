e02c2dcd7d5183559c33f72fd421388782c6eb70
Luis V <89930344+alejjuuu@users.noreply.github.com>
2023-11-06 22:39:38 -0500

    new changes on main page

--- midterm/next.config.js
+++ midterm/next.config.js
@@ -1,8 +1,21 @@
+const isProd = process.env.NODE_ENV === 'production';
+
 module.exports = {
+  // Base path for the application on GitHub Pages
+  basePath: isProd ? '/midterm-portfolio-alejjuuu' : '',
+  // Asset prefix for static assets
+  assetPrefix: isProd ? '/midterm-portfolio-alejjuuu/' : '',
+  
+  // Image optimization settings
   images: {
-    domains: ['media.licdn.com'],
-    unoptimized: true, // Disable the image optimization
+    unoptimized: true, // Disable Next.js' built-in image optimization
   },
-  assetPrefix: '/midterm-portfolio-alejjuuu/', // Adjusted for GitHub Pages
-  // ... other Next.js config
+
+  // Experimental features can change in future releases
+  experimental: {
+    output: 'standalone', // Enable static export in newer versions of Next.js
+  },
+
+  // Other configurations...
+  // Add any other configurations you might have here.
 };
